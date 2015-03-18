d97e178440e1be2a18c9e544315c7d36b8ac19e3
beecake <trina@trixietech.com>
2015-03-17 22:20:36 -0400

    hello.php

--- hello.php
+++ hello.php
@@ -1,15 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML1.0 Transitional//EN"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML1.0Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang"en" lang="en">
 <head>
-  <meta http-equiv="content-type"content="text/html; charset=utf-8" />
-  <title>Hello, World!</title>
+<meta http-equiv="Content-Type"content="text/html; charset=utf-8"/>
+<title>Greetings!</title>
 </head>
 <body>
-  <p>The following was created by PHP:
-    <?php
-    print "Hello, world!";
-    ?>
-  </p>
+<?php // Script 3.7 - hello.php
+
+ini_set ('display_errors', 1); //Let me learn from my mistakes!
+error_reporting (E_ALL | E_STRICT); Show all possible problems!
+
+// This page should receive a name value in the URL.
+
+// Say "Hello.":
+$name = $_GET['name'];
+print "<p>Hello, <spanstyle=\"font-weight: bold;\">$name</span>!</p>";
+
+?>
 </body>
-</html>"
+</html>
