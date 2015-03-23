061c89d46a8510dfb614b94335c3646ec06cd0f1
beecake <trina@trixietech.com>
2015-03-22 22:23:09 -0400

    handle_calc.php

--- handle_calc.php
+++ handle_calc.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html PUBLIC "-///W3C//DTD XHTML1.0Transitional//EN"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML1.0Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <meta http-equiv="Content-Type"content="text/html; charset=utf-8"/>
 <title>Product Cost Calculator</title>
-<style type="text/ccs" media="screen">
+<style type="text/css" media="screen">
 .number { font-weight: bold;}
 </style>
 </head>
 <body>
-<?php // Script 4.4 - handle_calc.php #3
+<?php // Script 4.3 - handle_calc.php #4
 /* This script takes values from calculator.html and performs
 total cost and monthly payment calculations. */
 
@@ -23,11 +23,12 @@ $tax = $_POST['tax'];
 $shipping = $_POST['shipping'];
 $payments = $_POST['payments'];
 
-//Calculate the total:
+// Calculate the total:
 $total = (($price * $quantity) + $shipping) - $discount;
 
 // Determine the tax rate:
-$taxrate = ($tax/100) + 1;
+$taxrate = $tax/100;
+$taxrate++;
 
 // Factor in the tax rate:
 $total = $total * $taxrate;
@@ -47,7 +48,7 @@ $<span class=\"number\">$shipping</span> shipping cost and a <br />
 <span class=\"number\">$tax</span> percent tax rate.<br />
 After your $<span class=\"number\">$discount</span> discount, the total cost is
 $<span class=\"number\">$total</span>.<br />
-Divided over <span class=\"number\">$payments</span> monthly payments, that would be $<span class=\"number\">$monthly</span> each.</p>";
+Divided over <span class=\"number\">$payments</span> monthly payments, that would be $<span class=\"number\">$monthly</span> each.</p>;
 
 ?>
 </body>
